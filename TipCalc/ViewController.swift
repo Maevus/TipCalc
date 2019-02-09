2d405ae2e3e69fe631914e6bce0fa003da35833b
Maeve Lynskey <maevus@Maeves-MacBook-Air.local>
2019-02-09 12:12:52 +0000

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Maeve Lynskey on 09/02/2019.
+//  Copyright © 2019 COMP47390. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
