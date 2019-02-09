d21fef55184c0beb41721a1c504bd1a26bb4022b
Maeve Lynskey <maevus@Maeves-MacBook-Air.local>
2019-02-09 15:57:46 +0000

    initial ui setup

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountLabel: UILabel!
+    @IBOutlet weak var customTipPercentLabel1: UILabel!
+    @IBOutlet weak var customTipPercentageSlider: UISlider!
+    @IBOutlet weak var customTipPercentLabel2: UILabel!
+    @IBOutlet weak var tip18Label: UILabel!
+    @IBOutlet weak var total18Label: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var tip15Label: UILabel!
+    @IBOutlet weak var total15Label: UILabel!
+    let decimal100 = NSDecimalNumber(string: "100.0")
+    let decimal15percent = NSDecimalNumber(string: "0.15")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        inputTextField.becomeFirstResponder()
+        
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        
+    }
+    
+    
 }
 
